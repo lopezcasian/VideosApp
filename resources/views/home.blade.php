a2eb26a090065fae3d149f1cb82002b73783550e
Esteban Lopez <estebanlopez.cn@gmail.com>
2018-10-03 11:19:24 -0500

    List all the videos in the home view.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="container">
+            @if (session('message'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('message') }}
                 </div>
-            </div>
+            @endif
+
+            <ul id="videos-list">
+                @foreach($videos as $video)
+                    <li class="video-item col-md-4 pull-left">
+                        <!-- Video image -->
+                        <div class="data">
+                            <h4>{{ $video->title }}</h4>
+                        </div>
+
+                        <!-- Action buttons -->
+                    </li>
+                @endforeach
+            </ul>
         </div>
+        {{ $videos->links() }}
     </div>
 </div>
 @endsection
